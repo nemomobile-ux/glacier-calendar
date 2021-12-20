5a28cda3fee386ce3cc5f3351649efe413a9a09c
Jozef Mlich <jmlich83@gmail.com>
2021-12-20 15:33:39 +0000

    lupdate

---
+++ translations/glacier-calendar.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AddEventPage</name>
+    <message>
+        <location filename="../src/qml/pages/AddEventPage.qml" line="30"/>
+        <source>Add event</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CalendarViewPage</name>
+    <message>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="39"/>
+        <source>Calendar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="59"/>
+        <source>day</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="63"/>
+        <source>week</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="67"/>
+        <source>month</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="71"/>
+        <source>year</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="75"/>
+        <source>agena</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="42"/>
+        <source>Calendar</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../src/qml/pages/SettingsPage.qml" line="30"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
