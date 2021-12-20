d4c6369361885b59a020fa489bd2238663344267
Jozef Mlich <jmlich83@gmail.com>
2021-12-20 15:49:26 +0000

    transifex needs some reasonable language for source translation and plurals

--- translations/glacier-calendar.ts
+++ translations/glacier-calendar.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en_US">
 <context>
     <name>AddEventPage</name>
     <message>
@@ -17,27 +17,27 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/pages/CalendarViewPage.qml" line="59"/>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="60"/>
         <source>day</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/pages/CalendarViewPage.qml" line="63"/>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="65"/>
         <source>week</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/pages/CalendarViewPage.qml" line="67"/>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="70"/>
         <source>month</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/pages/CalendarViewPage.qml" line="71"/>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="75"/>
         <source>year</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/pages/CalendarViewPage.qml" line="75"/>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="80"/>
         <source>agenda</source>
         <translation type="unfinished"></translation>
     </message>
