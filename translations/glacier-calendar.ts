a847dbc8c8d3b5d93c14e5d7bb4983ebb00b547c
Jozef Mlich <jmlich83@gmail.com>
2022-02-02 20:46:25 +0100

    lupdate

--- translations/glacier-calendar.ts
+++ translations/glacier-calendar.ts
@@ -5,36 +5,48 @@
     <name>AddEventPage</name>
     <message>
         <location filename="../src/qml/pages/AddEventPage.qml" line="38"/>
+        <location filename="../src/qml/pages/AddEventPage.qml" line="38"/>
         <source>Add event</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/pages/AddEventPage.qml" line="61"/>
-        <location filename="../src/qml/pages/AddEventPage.qml" line="114"/>
+        <location filename="../src/qml/pages/AddEventPage.qml" line="61"/>
         <source>Summary</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/AddEventPage.qml" line="75"/>
         <location filename="../src/qml/pages/AddEventPage.qml" line="75"/>
         <source>Start</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/pages/AddEventPage.qml" line="90"/>
+        <location filename="../src/qml/pages/AddEventPage.qml" line="90"/>
         <source>End</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/pages/AddEventPage.qml" line="106"/>
+        <location filename="../src/qml/pages/AddEventPage.qml" line="106"/>
         <source>All day</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/AddEventPage.qml" line="114"/>
+        <location filename="../src/qml/pages/AddEventPage.qml" line="114"/>
+        <source>Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/AddEventPage.qml" line="128"/>
         <location filename="../src/qml/pages/AddEventPage.qml" line="128"/>
         <source>Location</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/AddEventPage.qml" line="143"/>
         <location filename="../src/qml/pages/AddEventPage.qml" line="143"/>
         <source>Save event</source>
         <translation type="unfinished"></translation>
@@ -44,30 +56,36 @@
     <name>CalendarViewPage</name>
     <message>
         <location filename="../src/qml/pages/CalendarViewPage.qml" line="39"/>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="39"/>
         <source>Calendar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="60"/>
         <location filename="../src/qml/pages/CalendarViewPage.qml" line="60"/>
         <source>day</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/pages/CalendarViewPage.qml" line="65"/>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="65"/>
         <source>week</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="70"/>
         <location filename="../src/qml/pages/CalendarViewPage.qml" line="70"/>
         <source>month</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/pages/CalendarViewPage.qml" line="75"/>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="75"/>
         <source>year</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/CalendarViewPage.qml" line="80"/>
         <location filename="../src/qml/pages/CalendarViewPage.qml" line="80"/>
         <source>agenda</source>
         <translation type="unfinished"></translation>
@@ -85,6 +103,7 @@
     <name>SettingsPage</name>
     <message>
         <location filename="../src/qml/pages/SettingsPage.qml" line="30"/>
+        <location filename="../src/qml/pages/SettingsPage.qml" line="30"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
@@ -92,6 +111,7 @@
 <context>
     <name>agendaViewPage</name>
     <message>
+        <location filename="../src/qml/pages/agendaViewPage.qml" line="50"/>
         <location filename="../src/qml/pages/agendaViewPage.qml" line="50"/>
         <source>No events</source>
         <translation type="unfinished"></translation>
